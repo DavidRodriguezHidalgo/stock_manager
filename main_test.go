fdee7ee294a24b524a18c091810c96d7ab8b6ec4
David Rodriguez <denzilrh90@gmail.com>
2022-06-24 14:17:45 +0200

    adding tests for gorm

---
+++ main_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+
+	"./models"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+
+	"testing"
+)
+
+func TestDbOperations(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	db.AutoMigrate(&models.StockCompany{})
+
+
+	companyModel := models.StockCompany{}
+	company := models.StockCompanySql{Db: db, Sc: companyModel}
+	company1 := models.StockCompany{Ticker: "ZOT", NumberOfStocks: 100}
+	company.Create(company1)
+	fmt.Println(company.GetByTicker("ZOT"))
+	company.Update("ZOT", models.StockCompany{NumberOfStocks: 200})
+	fmt.Println(company.GetByTicker("ZOT"))
+	company.Delete(company.GetByTicker("ZOT"))
+}
